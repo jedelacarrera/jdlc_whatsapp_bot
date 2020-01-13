c737a93468beca4b7d6a3cd4a8e01002235945cf
jedelacarrera <jedelacarrera@uc.cl>
2020-01-13 01:27:31 -0300

    :bug: fix no index

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'el-secreto'
 app.config['JWT_SECRET_KEY'] = 'el-secreto'
 
-@app.route('/new_message', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    return jsonify({'status': True, 'route': 'index'})
+
+@app.route('/new_message', methods=['GET', 'POST'])
+def new_message():
     print(request)
     print(request.args)
     print(request.form)
