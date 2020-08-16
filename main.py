ea1a5fc007f99aca4795e9ffc413d873f8a6b0af
jedelacarrera <jedelacarrera@uc.cl>
2020-08-16 17:44:39 -0400

    fix: :bug: return string after cron job

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
 @app.route("/cron_job", methods=["POST"])
 def cron_job_trigger():
     cron_function()
+    return "Cron job run succesfully", 200
 
 
 @app.route("/", methods=["GET", "POST"])
